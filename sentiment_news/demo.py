8d55a3571af1beebfaf0e1e211e31f66cf2e6bbf
draftbk <971143573@qq.com>
2017-04-02 16:42:02 +0800

    完成循环读取文件夹中文件

--- sentiment_news/demo.py
+++ sentiment_news/demo.py
@@ -71,13 +71,11 @@ def singleTxt(txtname):
 
 
 path = sys.argv[1] #文件夹目录
-print path
 files= os.listdir(path) #得到文件夹下的所有文件名称
-print files
 articles={}
 for file in files: #遍历文件夹
      if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
-         data = singleTxt(file)
+         data = singleTxt(path+'/'+file)
          articles[file] = data
 json_str = json.dumps(articles)
 print(json_str) #打印结果
